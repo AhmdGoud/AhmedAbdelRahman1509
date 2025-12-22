564fb0fa86a52df167418012b1899da210eea223
ahmed gouda <ag021055@gmail.com>
2025-12-22 13:46:05 +0400

    linked email and and uploaded resume

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// still has to be done
+// 1- conatct info
+// 2- upload resume
+
 const mode = document.querySelector('.mode')
 mode.addEventListener('click', function(){
     document.body.classList.toggle('theme')
